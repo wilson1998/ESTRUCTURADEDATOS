f03efdafed14d3311a422387f9960e757e596938
Wilson Liñeiro <36859716+wilson1998@users.noreply.github.com>
2023-02-09 19:01:06 -0500

    Ejemplo 1
    Este ejemplo es el primero en GitHub

---
+++ Sin_nombre1.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <conio.h>
+#include <stdio.h>
+#include <windows.h>
+using namespace std;
+
+
+int main()
+{
+   gotoxy(20,3);
+   cout<<"Text... 20, 3";
+   getch();
+}
+
